c31cfa73d7cbfd4fa7d659cde24569b70d8ab55b
caspian boiler <aliyabbjni@gmail.com>
2026-09-17 00:51:15 +0330

    Update prices.js

--- prices.js
+++ prices.js
@@ -1,21 +1,16 @@
 window.EMBEDDED_PRICES = {
   version: '2025-01-15',
-  currency: 'تومان',
   sheets: {
-    'A516-70_6':  45000, 'A516-70_8':  44000, 'A516-70_10': 43000,
-    'A516-70_12': 43500, 'A516-70_14': 44500,
-    'SS304_6':    185000, 'SS304_8':   180000,
-    'SS316_6':    220000, 'SS316_8':   215000
+    'A516-70_6':45000,'A516-70_8':44000,'A516-70_10':43000,
+    'A516-70_12':43500,'A516-70_14':44500,
+    'SS304_6':185000,'SS304_8':180000,'SS316_6':220000,'SS316_8':215000
   },
   dish_markup_pct: 35,
   pipe: { copper_1_2: 850000, copper_3_4: 1250000, copper_1: 1650000 },
-  flange_1inch: 450000, flange_2inch: 780000,
-  flange_3inch: 1350000, flange_4inch: 1850000,
-  manhole_16: 4500000,
-  electrode_kg: 320000,
-  welder_hr: 280000, fitter_hr: 200000, painter_hr: 180000,
-  paint_epoxy_m2: 180000, paint_zinc_m2: 220000,
-  insul_ceramic_50_m2: 850000, insul_ceramic_100_m2: 1450000,
-  consumables_pct: 8,
-  transport_per_kg: 350
+  flange_1inch:450000, flange_2inch:780000, flange_3inch:1350000, flange_4inch:1850000,
+  manhole_16:4500000, electrode_kg:320000,
+  welder_hr:280000, fitter_hr:200000, painter_hr:180000,
+  paint_epoxy_m2:180000, paint_zinc_m2:220000,
+  insul_ceramic_50_m2:850000, insul_ceramic_100_m2:1450000,
+  consumables_pct:8, transport_per_kg:350
 };
