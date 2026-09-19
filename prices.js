e250fd1ce5b45367a5da685ef030badd39d4de79
caspian boiler <aliyabbjni@gmail.com>
2026-09-19 18:06:10 +0330

    Update prices.js

--- prices.js
+++ prices.js
@@ -4,132 +4,76 @@ window.EMBEDDED_PRICES = {
   currency: 'تومان',
 
   sheets: {
-    'ST37_2.5': 72000, 'ST37_3': 108455, 'ST37_4': 108000,
-    'ST37_5': 108000, 'ST37_6': 60272,
-    'S235JR_5': 95000, 'S235JR_6': 93000, 'S235JR_8': 92000,
-    'S235JR_10': 91000, 'S235JR_12': 90000,
-    'A516-70_8': 128000, 'A516-70_10': 123000, 'A516-70_12': 120500,
-    'A516-70_14': 120500, 'A516-70_15': 123000,
-    'Galvanized_3': 201100, 'Galvanized_4': 194000, 'Galvanized_5': 190000,
-    'Galvanized_6': 188000, 'Galvanized_8': 185000,
-    'Galvanized_10': 180000, 'Galvanized_12': 175000,
-    'SS304_3': 280000, 'SS304_4': 275000, 'SS304_5': 270000,
-    'SS304_6': 265000, 'SS304_8': 260000, 'SS304_10': 255000,
-    'SS316_3': 320000, 'SS316_4': 315000, 'SS316_5': 310000,
-    'SS316_6': 305000, 'SS316_8': 300000, 'SS316_10': 295000
+    'ST37_2.5':72000,'ST37_3':108455,'ST37_4':108000,'ST37_5':108000,'ST37_6':60272,
+    'S235JR_5':95000,'S235JR_6':93000,'S235JR_8':92000,'S235JR_10':91000,'S235JR_12':90000,
+    'A516-70_8':128000,'A516-70_10':123000,'A516-70_12':120500,'A516-70_14':120500,'A516-70_15':123000,
+    'Galvanized_3':201100,'Galvanized_4':194000,'Galvanized_5':190000,'Galvanized_6':188000,
+    'Galvanized_8':185000,'Galvanized_10':180000,'Galvanized_12':175000,
+    'SS304_3':280000,'SS304_4':275000,'SS304_5':270000,'SS304_6':265000,'SS304_8':260000,'SS304_10':255000,
+    'SS316_3':320000,'SS316_4':315000,'SS316_5':310000,'SS316_6':305000,'SS316_8':300000,'SS316_10':295000
   },
 
-  // تمام لوله‌ها به تومان/شاخه ۶ متر
   pipes: {
-    'galv_3_4': 19200000, 'galv_1': 22300000, 'galv_1_1_4': 24500000,
-    'galv_1_1_2': 28000000, 'galv_2': 35000000, 'galv_2_1_2': 45000000,
-    'galv_3': 60000000, 'galv_4': 85000000,
-    'seamless40_3_4': 3200000, 'seamless40_1': 4810508, 'seamless40_1_1_4': 6200000,
-    'seamless40_1_1_2': 7500000, 'seamless40_2': 9000000, 'seamless40_2_1_2': 11500000,
-    'seamless40_3': 14000000, 'seamless40_4': 20000000,
-    'seamless40_6': 32000000, 'seamless40_8': 44189090,
-    'seamless80_3_4': 3800000, 'seamless80_1': 4450000, 'seamless80_1_1_4': 5800000,
-    'seamless80_1_1_2': 7200000, 'seamless80_2': 8800000, 'seamless80_2_1_2': 12000000,
-    'seamless80_3': 15000000, 'seamless80_4': 22000000,
-    'seamless80_6': 38000000, 'seamless80_8': 50000000,
-    'firetube_1': 2500000, 'firetube_1_1_2': 3500000, 'firetube_2': 4500000,
-    'SS304_3_4': 4800000, 'SS304_1': 6500000, 'SS304_1_1_4': 8200000,
-    'SS304_1_1_2': 9800000, 'SS304_2': 12500000, 'SS304_2_1_2': 15500000,
-    'SS304_3': 18500000, 'SS304_4': 26000000,
-    'SS309_3_4': 5600000, 'SS309_1': 7500000, 'SS309_1_1_4': 9500000,
-    'SS309_1_1_2': 11500000, 'SS309_2': 14500000, 'SS309_2_1_2': 18500000,
-    'SS309_3': 22000000, 'SS309_4': 30000000,
-    'copper_1_2': 5100000, 'copper_3_4': 7500000, 'copper_1': 9900000,
-    'copper_1_1_4': 12600000
+    'galv_3_4':19200000,'galv_1':22300000,'galv_1_1_4':24500000,'galv_1_1_2':28000000,
+    'galv_2':35000000,'galv_2_1_2':45000000,'galv_3':60000000,'galv_4':85000000,
+    'seamless40_3_4':3200000,'seamless40_1':4810508,'seamless40_1_1_4':6200000,
+    'seamless40_1_1_2':7500000,'seamless40_2':9000000,'seamless40_2_1_2':11500000,
+    'seamless40_3':14000000,'seamless40_4':20000000,'seamless40_6':32000000,'seamless40_8':44189090,
+    'seamless80_3_4':3800000,'seamless80_1':4450000,'seamless80_1_1_4':5800000,
+    'seamless80_1_1_2':7200000,'seamless80_2':8800000,'seamless80_2_1_2':12000000,
+    'seamless80_3':15000000,'seamless80_4':22000000,'seamless80_6':38000000,'seamless80_8':50000000,
+    'firetube_1':2500000,'firetube_1_1_2':3500000,'firetube_2':4500000,
+    'SS304_3_4':4800000,'SS304_1':6500000,'SS304_1_1_4':8200000,'SS304_1_1_2':9800000,
+    'SS304_2':12500000,'SS304_2_1_2':15500000,'SS304_3':18500000,'SS304_4':26000000,
+    'SS309_3_4':5600000,'SS309_1':7500000,'SS309_1_1_4':9500000,'SS309_1_1_2':11500000,
+    'SS309_2':14500000,'SS309_2_1_2':18500000,'SS309_3':22000000,'SS309_4':30000000,
+    'copper_1_2':5100000,'copper_3_4':7500000,'copper_1':9900000,'copper_1_1_4':12600000
   },
 
   bushings: {
-    'bush_1_2': 86900, 'bush_3_4': 140000, 'bush_1': 160000,
-    'bush_1_1_4': 200000, 'bush_1_1_2': 250000, 'bush_2': 390000,
-    'bush_2_1_2': 600000, 'bush_3': 900000, 'bush_4': 3360000
+    'bush_1_2':86900,'bush_3_4':140000,'bush_1':160000,'bush_1_1_4':200000,
+    'bush_1_1_2':250000,'bush_2':390000,'bush_2_1_2':600000,'bush_3':900000,'bush_4':3360000
   },
 
   flanges: {
-    'flange_1_2': 220000, 'flange_3_4': 300000, 'flange_1': 385000,
-    'flange_1_1_4': 480000, 'flange_1_1_2': 550000, 'flange_2': 780000,
-    'flange_2_1_2': 1050000, 'flange_3': 1350000, 'flange_4': 2160000,
-    'flange_6': 3500000, 'flange_8': 5200000
+    'flange_1_2':220000,'flange_3_4':300000,'flange_1':385000,'flange_1_1_4':480000,
+    'flange_1_1_2':550000,'flange_2':780000,'flange_2_1_2':1050000,
+    'flange_3':1350000,'flange_4':2160000,'flange_6':3500000,'flange_8':5200000
   },
 
-  manholes: {
-    'manhole_16': 8500000, 'manhole_18': 12000000, 'manhole_20': 16000000
-  },
-  headholes: { 'headhole_6': 3500000, 'headhole_8': 5200000 },
-  handholes: { 'handhole_4': 1800000, 'handhole_5': 2500000 },
+  manholes: {'manhole_16':8500000,'manhole_18':12000000,'manhole_20':16000000},
+  headholes: {'headhole_6':3500000,'headhole_8':5200000},
+  handholes: {'handhole_4':1800000,'handhole_5':2500000},
 
-  insulation: {
-    'ceramic_1_5': 350000, 'ceramic_2_5': 723000,
-    'ceramic_5': 723000, 'ceramic_12': 1200000
-  },
+  insulation: {'ceramic_1_5':350000,'ceramic_2_5':723000,'ceramic_5':723000,'ceramic_12':1200000},
+  covers: {'steel_1x2':850000,'steel_1.25x2.45':1300000,'aluminum_1x2':350000,'aluminum_1.25x2.45':540000},
 
-  covers: {
-    'steel_1x2': 850000, 'steel_1.25x2.45': 1300000,
-    'aluminum_1x2': 350000, 'aluminum_1.25x2.45': 540000
-  },
-
-  // وزن لوله‌ها kg/m
   pipeWeights: {
-    'galv_3_4': 2.4, 'galv_1': 3.4, 'galv_1_1_4': 4.6, 'galv_1_1_2': 5.4,
-    'galv_2': 7.5, 'galv_2_1_2': 11, 'galv_3': 15, 'galv_4': 22,
-    'seamless_3_4': 1.7, 'seamless_1': 3.2, 'seamless_1_1_4': 4.3,
-    'seamless_1_1_2': 5.2, 'seamless_2': 7.5, 'seamless_2_1_2': 11,
-    'seamless_3': 15, 'seamless_4': 22, 'seamless_6': 40, 'seamless_8': 60,
-    'firetube_1': 4.2, 'firetube_1_1_2': 6.5, 'firetube_2': 9.5,
-    'SS304_3_4': 1.6, 'SS304_1': 2.6, 'SS304_1_1_4': 3.7,
-    'SS304_1_1_2': 4.8, 'SS304_2': 7.0, 'SS304_2_1_2': 10,
-    'SS304_3': 15, 'SS304_4': 22,
-    'SS309_3_4': 1.6, 'SS309_1': 2.6, 'SS309_1_1_4': 3.7,
-    'SS309_1_1_2': 4.8, 'SS309_2': 7.0, 'SS309_2_1_2': 10,
-    'SS309_3': 15, 'SS309_4': 22,
-    'copper_1_2': 0.9, 'copper_3_4': 1.4, 'copper_1': 2.2, 'copper_1_1_4': 3.2
+    'galv_3_4':2.4,'galv_1':3.4,'galv_1_1_4':4.6,'galv_1_1_2':5.4,'galv_2':7.5,'galv_2_1_2':11,'galv_3':15,'galv_4':22,
+    'seamless_3_4':1.7,'seamless_1':3.2,'seamless_1_1_4':4.3,'seamless_1_1_2':5.2,'seamless_2':7.5,
+    'seamless_2_1_2':11,'seamless_3':15,'seamless_4':22,'seamless_6':40,'seamless_8':60,
+    'firetube_1':4.2,'firetube_1_1_2':6.5,'firetube_2':9.5,
+    'SS304_3_4':1.6,'SS304_1':2.6,'SS304_1_1_4':3.7,'SS304_1_1_2':4.8,'SS304_2':7.0,
+    'SS304_2_1_2':10,'SS304_3':15,'SS304_4':22,
+    'SS309_3_4':1.6,'SS309_1':2.6,'SS309_1_1_4':3.7,'SS309_1_1_2':4.8,'SS309_2':7.0,
+    'SS309_2_1_2':10,'SS309_3':15,'SS309_4':22,
+    'copper_1_2':0.9,'copper_3_4':1.4,'copper_1':2.2,'copper_1_1_4':3.2
   },
 
   equipmentParts: {
-    cathodic: { anode_zn: 350000, anode_al: 420000, anode_mg: 550000 },
-    ladder: {
-      price_per_kg: 80000,
-      weight_per_m: 15,
-      pipe_30x30: 4.2, pipe_40x40: 6.5, pipe_50x50: 9.8
-    },
-    softener: {
-      resin_kg: 180000,
-      control_valve_auto: 12000000,
-      control_valve_semi: 7500000,
-      control_valve_manual: 3500000
-    },
-    sandFilter: {
-      auto_valve: 8500000
-    },
-    deaerator: {
-      tower_shell_kg: 220000,
-      tower_head_kg: 250000
-    },
-    expansion: {
-      diaphragm_butyl: 4500000,
-      diaphragm_epdm: 3200000
-    },
-    vessel: {
-      saddle_support: 1800000,
-      leg_support: 1200000,
-      base_pad_price: 250000,
-      base_pad_weight_kg: 3,
-      pad_price_per_kg: 55000
-    }
+    cathodic: {anode_zn:350000,anode_al:420000,anode_mg:550000},
+    ladder: {price_per_kg:80000,weight_per_m:15,pipe_30x30:4.2,pipe_40x40:6.5,pipe_50x50:9.8},
+    softener: {resin_kg:180000,control_valve_auto:12000000,control_valve_semi:7500000,control_valve_manual:3500000},
+    vessel: {base_pad_price:250000,base_pad_weight_kg:3}
   },
 
-  dish_markup_pct: 35,
-  electrode_kg: 320000,
-  welder_hr: 280000,
-  fitter_hr: 200000,
-  painter_hr: 180000,
-  paint_epoxy_m2: 180000,
-  paint_zinc_m2: 220000,
-  consumables_pct: 8,
-  transport_per_kg: 350,
-  pdf_header: 'Caspian Mobadel Amard — کاسپین مبدل آمارد'
+  dish_markup_pct:35,
+  electrode_kg:320000,
+  welder_hr:280000,
+  fitter_hr:200000,
+  painter_hr:180000,
+  paint_epoxy_m2:180000,
+  paint_zinc_m2:220000,
+  consumables_pct:8,
+  transport_per_kg:350
 };
